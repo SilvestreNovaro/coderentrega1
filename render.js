6f7fc6dc367fc753bd110f3c7ebb3c087ffadd75
silve <silvestrenovarohueyo@gmail.com>
2022-10-29 23:44:33 -0300

    entrega final

---
+++ render.js
@@ -0,0 +1,33 @@
+const camadaPerros = [
+    {
+        "id": 1,
+        "nombre": 'Limon',
+        "sexo": "macho",
+        "imagen": 'id1.jpg'
+    },
+    {
+        "id": 2,
+        "nombre": 'Maggie',
+        "sexo": "hembra",
+        "imagen": 'id2.jpg'
+    },
+    {
+        "id": 3,
+        "nombre": 'Tango',
+        "sexo": "macho",
+        "imagen": 'id3.jpg'
+    },
+    {
+        "id": 4,
+        "nombre": 'Ronnie',
+        "sexo": "macho",
+        "imagen": 'id4.jpg'
+    },
+    {
+        "id": 4,
+        "nombre": 'Victoria',
+        "sexo": "hembra",
+        "imagen": 'id5.jpg'
+    }
+
+];
--- script.js
+++ script.js
@@ -9,7 +9,7 @@ bienvenida();
 
 
 const verPerros = () =>{
-    let confirmacion = prompt("¿Queres conocer a la camada disponible?")
+    let confirmacion = confirm("¿Queres conocer a la camada disponible?")
     if(confirmacion){
         const perros = ["Limon", "Maggie", "Tango", "Ronnie", "Victoria"];
 
@@ -23,7 +23,7 @@ const verPerros = () =>{
     }
 }
 
-verPerros();
+// verPerros();
 
 // const form = document.querySelector("#form");
 // form.addEventListener("submit", function(e){
@@ -31,69 +31,90 @@ verPerros();
 // })
 
 
-let botonVerMas = document.createElement("button");
-let botonTexto = document.createTextNode("Conocer camada disponible");
-botonVerMas.appendChild(botonTexto);
-document.body.appendChild(botonVerMas);
+// let botonVerMas = document.createElement("button");
+// let botonTexto = document.createTextNode("Conocer camada disponible");
+// botonVerMas.appendChild(botonTexto);
+// document.body.appendChild(botonVerMas);
 
-botonVerMas.addEventListener("click", function(){
-alert("No hay camada disponible aun");    
-})
+// botonVerMas.addEventListener("click", function(){
+// alert("No hay camada disponible aun");    
+// })
 
 
 // CARRITO
-const camadaPerros = [
-    {
-        id: 1,
-        nombre: 'Limon',
-        sexo: "macho",
-        imagen: 'id1.jpg'
-    },
-    {
-        id: 2,
-        nombre: 'Maggie',
-        sexo: "hembra",
-        imagen: 'id2.jpg'
-    },
-    {
-        id: 3,
-        nombre: 'Tango',
-        sexo: "macho",
-        imagen: 'id3.jpg'
-    },
-    {
-        id: 4,
-        nombre: 'Ronnie',
-        sexo: "macho",
-        imagen: 'id4.jpg'
-    },
-    {
-        id: 4,
-        nombre: 'Victoria',
-        sexo: "hembra",
-        imagen: 'id5.jpg'
-    }
 
-];
+
+const containerPerros = document.createElement("div");
+const perrosContainer = document.querySelector("#perros-container");
+perrosContainer.addEventListener("click",renderizarPerros);
+    // e.preventDefault();
+    
+
+
+function renderizarPerros(camadaPerros) {
+    camadaPerros.forEach(perro => {
+        containerPerros.innerHTML += `<div class="card">
+        <h5 class="card-title">${perro.nombre}</h5>
+        <p class="card-text">$<span>${perro.precio}</span></p>
+        <img src ="${perro.imagen}"></img>
+        <button type="button" class="btn-comprar">Añadir al carrito</button>
+    </div>
+    `
+});
+}      
 
 
 
 
-let carrito = [];
-const DOMitems = document.querySelector('#items');
-const DOMcarrito = document.querySelector('#carrito');
-const DOMtotal = document.querySelector('#total');
-const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+// let carrito = [];
+// const DOMitems = document.querySelector('#items');
+// const DOMcarrito = document.querySelector('#carrito');
+// const DOMtotal = document.querySelector('#total');
+// const DOMbotonVaciar = document.querySelector('#boton-vaciar');
 
 
 //FUNCION PARA VACIAR EL CARRITO
 
 
-function vaciarCarrito() {
-    // Limpiamos los productos guardados
-    carrito = [];
-    // Renderizamos los cambios
-    renderizarCarrito();
-    //ESTA FUNCION AUN NO LA CREO.
- }
+// function vaciarCarrito() {
+//     // Limpiamos los productos guardados
+//     carrito = [];
+//     // Renderizamos los cambios
+//     renderizarCarrito();
+//     //ESTA FUNCION AUN NO LA CREO.
+//  }
+
+
+
+ // MODAL FORMULARIO
+const btnLanzarModal = document.querySelector("#lanzar-modal")
+const btnOcultarModal = document.querySelector("#ocultar-modal")
+
+const contModal = document.querySelector(".contenedor-modal")
+
+
+const nombreInput = document.querySelector("#nombre")
+const cachorroInput = document.querySelector("#cachorro")
+
+btnLanzarModal.addEventListener("click", (e) =>{
+    e.preventDefault();
+    contModal.classList.add("mostrar")
+})
+
+btnOcultarModal.addEventListener("click", (e) =>{
+    e.preventDefault();
+    contModal.classList.remove("mostrar");
+
+    const adoptante = document.querySelector(".contenedor");
+
+    const aNombre = document.createElement("p");
+    aNombre.textContent = nombreInput.value;
+    const pCachorro = document.createElement("p");
+    pCachorro.textContent +="Has elgido adoptar a " + cachorroInput.value;
+    
+    adoptante.appendChild(aNombre);
+    adoptante.appendChild(pCachorro);
+
+});
+
  
